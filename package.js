adae4ed7d549ef39416643b7e806d4add6762dc2
DougThwaites <hello@dougthwaites.co.nz>
2015-08-29 20:16:26 +1200

    Initial Commit

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'dougthwaites:jquery-match-height',
+  version: '0.0.1',
+  summary: 'matchHeight makes the height of all selected elements exactly equal.',
+  git: 'https://github.com/DougThwaites/jquery-match-height',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+
+  api.versionsFrom('1.1.0.3');
+
+  api.use([
+    'aldeed:template-extension@3.4.3',
+    'templating',
+  ], ['client']);
+ 
+  api.addFiles([
+    'src/jquery.matchHeight.js',
+    'lib/init.js',
+  ], ['client']);
+
+});
